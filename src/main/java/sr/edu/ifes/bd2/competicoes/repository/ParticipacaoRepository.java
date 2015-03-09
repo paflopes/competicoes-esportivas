fa3d63be361a5a50e63f783e7a14c005ce1464de
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-03-09 14:40:39 -0300

    Consulta das SPs

--- src/main/java/sr/edu/ifes/bd2/competicoes/repository/ParticipacaoRepository.java
+++ src/main/java/sr/edu/ifes/bd2/competicoes/repository/ParticipacaoRepository.java
@@ -1,6 +1,8 @@
 package sr.edu.ifes.bd2.competicoes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sr.edu.ifes.bd2.competicoes.model.Participacao;
 import sr.edu.ifes.bd2.competicoes.repository.custom.participacao.ParticipacaoRepositoryCustom;
@@ -13,4 +15,10 @@ public interface ParticipacaoRepository extends JpaRepository<Participacao, Long
     public List<Participacao> findByCompetidorId(Long id);
 
     public List<Participacao> findByCompeticaoId(Long id);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM criarpodio(:idCompeticao)")
+    public List<Participacao> criarPodio( @Param("idCompeticao") Long idCompeticao);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM atualizarpontos(?1, ?2, ?3, ?4)")
+    public List<Participacao> atualizarPontos(Long idCompeticao, Integer ptsVitoria, Integer ptsEmpate, Integer ptsDerrota);
 }
